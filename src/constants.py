097cc83d3eac8f7912a36b45dc757ccc9cc4aa17
Insality <insality@gmail.com>
2014-08-01 23:35:23 +0600

    Added base structure

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+__author__ = 'Insality'
+
+# Game consts
+GAME_NAME = "Pyrox"
+
+# Map consts
+TILE_WIDTH = 24
+TILE_HEIGHT = 24
+
+# Window consts
+WINDOW_WIDTH = 720
+WINDOW_HEIGHT = 480
---
+++ src/resource.py
@@ -0,0 +1,13 @@
+__author__ = 'Insality'
+
+import pyglet
+
+pyglet.resource.path = ["../res/"]
+pyglet.resource.path = ["../res/font/"]
+pyglet.resource.path = ["../res/icon/"]
+pyglet.resource.path = ["../res/image/"]
+pyglet.resource.path = ["../res/music/"]
+pyglet.resource.path = ["../res/sound/"]
+pyglet.resource.reindex()
+
+# window_icon = pyglet.resource.image('icon.png')
