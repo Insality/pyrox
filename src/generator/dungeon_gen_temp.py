a8e78b87d1d05ef4f2087e32b18c64d560892b84
Insality <insality@gmail.com>
2014-08-02 20:17:35 +0600

    Dungeon generator fixes

--- src/generator/dungeon_gen_temp.py
+++ src/generator/dungeon_gen_temp.py
@@ -5,11 +5,11 @@ import math
 from random import randint, choice
 
 
-
 class Hall:
     def __init__(self, from_point, to_point):
         self.x_from, self.y_from = from_point
         self.x_to, self.y_to = to_point
+
 
 class Room:
     def __init__(self, pos_x, pos_y, width, height):
@@ -33,7 +33,7 @@ class Room:
     def place_tile(self, x, y, tile):
         self.room[y][x] = tile
 
-    def make_random_door(self, direction):
+    def get_random_wall_pos(self, direction):
         dx, dy = 0, 0
         if (direction == UP or direction == DOWN):
             rnd = randint(1, self.width - 2)
@@ -43,15 +43,13 @@ class Room:
         if direction == UP:
             dx, dy = rnd, 0
         elif direction == DOWN:
-            dx, dy = rnd, self.height -1
+            dx, dy = rnd, self.height - 1
         elif direction == RIGHT:
             dx, dy = self.width - 1, rnd
         elif direction == LEFT:
             dx, dy = 0, rnd
 
-        print ("door", dx, dy)
-        # self.room[dy][dx] = TILE_DOOR
-        return dx+self.pos_x, dy+self.pos_y
+        return dx + self.pos_x, dy + self.pos_y
 
     def overlaps(self, other):
         return ( abs(self.center[0] - other.center[0]) < (self.width // 2) + (other.width // 2) and
@@ -68,30 +66,34 @@ class Dungeon:
         self.halls = []
         self.dungeon = []
 
-
     def generate(self):
         if len(self.rooms) <= 0:
-            self.make_room(0, 0, randint(ROOM_MIN_WIDTH, ROOM_MAX_WIDTH), randint(ROOM_MIN_HEIGHT, ROOM_MAX_HEIGHT))
+            self.make_first_room(0, 0, randint(ROOM_MIN_WIDTH, ROOM_MAX_WIDTH),
+                                 randint(ROOM_MIN_HEIGHT, ROOM_MAX_HEIGHT))
 
-        # while (len(self.rooms) <= self.room_count):
-        self.make_random_room()
-        self.make_random_room()
-        self.make_random_room()
-        self.make_random_room()
-        self.make_random_room()
-        self.make_random_room()
+        while (len(self.rooms) < self.room_count):
+            self.make_random_room()
 
+        self.compile()
 
     def make_random_room(self):
         assert len(self.rooms) > 0, "A main room must me created"
 
-        rooms = filter(lambda x: len(x.free_direction) > 0, self.rooms)
-        print("Aviable rooms: %i" % len(rooms))
+        prefer_direction = set()
+        if (self.width > self.height):
+            prefer_direction.add(UP)
+            prefer_direction.add(DOWN)
+        else:
+            prefer_direction.add(RIGHT)
+            prefer_direction.add(LEFT)
 
-        # room_from = choice(rooms)
+        rooms = filter(lambda x: len(x.free_direction) > 0 and len(set(x.free_direction) & prefer_direction) > 0,
+                       self.rooms)
+
         room_from = choice(rooms)
-        direction = choice(room_from.free_direction)
-        door_from = room_from.make_random_door(direction)
+        dirs = set(room_from.free_direction) & prefer_direction
+        direction = choice(list(dirs))
+        door_from = room_from.get_random_wall_pos(direction)
 
         room_new = None
         for i in range(GEN_MAX_TRY_PLACE_ROOM):
@@ -104,14 +106,14 @@ class Dungeon:
             room_y = door_new[1]
             if (direction == UP):
                 room_x -= room_width // 2
-                room_y -= room_height-1
+                room_y -= room_height - 1
             elif (direction == RIGHT):
                 room_y -= room_height // 2
             elif (direction == DOWN):
                 room_x -= room_width // 2
             elif (direction == LEFT):
                 room_y -= room_height // 2
-                room_x -= room_width-1
+                room_x -= room_width - 1
 
             room_new = Room(room_x, room_y, room_width, room_height)
             hall_new = Hall(door_from, door_new)
@@ -129,20 +131,18 @@ class Dungeon:
             return
 
         room_from.free_direction.remove(direction)
-        room_new.free_direction.remove((direction+2)% 4)
+        room_new.free_direction.remove((direction + 2) % 4)
 
         self.halls.append(hall_new)
         self.rooms.append(room_new)
-
+        self.compile()
 
     def make_dungeon(self):
         self.dungeon = [[TILE_SOLID for col in range(self.width)] for row in range(self.height)]
 
-    def make_room(self, x, y, width, height):
-        assert width >= ROOM_MIN_WIDTH, "Incorrect room width"
-        assert height >= ROOM_MIN_HEIGHT, "Incorrect room height"
-
+    def make_first_room(self, x, y, width, height):
         self.rooms.append(Room(x, y, width, height))
+        self.compile()
 
     def _is_empty(self, x, y, width, height):
         for i in range(y, y + height):
@@ -184,23 +184,20 @@ class Dungeon:
 
         self.make_dungeon()
 
-        print "offsets"
-        print self.room_offset_x
-        print self.room_offset_y
         for room in self.rooms:
             self.place_room(room, self.room_offset_x, self.room_offset_y)
 
         for hall in self.halls:
-            from_x = hall.x_from+self.room_offset_x
-            from_y = hall.y_from+self.room_offset_y
+            from_x = hall.x_from + self.room_offset_x
+            from_y = hall.y_from + self.room_offset_y
             to_y = hall.y_to + self.room_offset_y
             to_x = hall.x_to + self.room_offset_x
 
-            for x in range(from_x, to_x+1):
-                for y in range(from_y, to_y+1):
+            for x in range(from_x, to_x + 1):
+                for y in range(from_y, to_y + 1):
                     self.dungeon[y][x] = TILE_EMPTY
-            for x in range(from_x, to_x-1, -1):
-                for y in range(from_y, to_y-1, -1):
+            for x in range(from_x, to_x - 1, -1):
+                for y in range(from_y, to_y - 1, -1):
                     self.dungeon[y][x] = TILE_EMPTY
 
             self.dungeon[from_y][from_x] = TILE_DOOR
@@ -229,15 +226,15 @@ def get_new_pos_by_direction(x, y, len, direction):
     return x, y
 
 
-def create_dungeon(room_count):
+def test_generator(room_count):
     assert room_count >= 3, "Room_count must be more than 3"
 
     dungeon = Dungeon(room_count)
 
     dungeon.generate()
-    dungeon.compile()
     dungeon.draw()
+    print(dungeon.width, dungeon.height)
 
 
 if __name__ == "__main__":
-    create_dungeon(4)
+    test_generator(7)
